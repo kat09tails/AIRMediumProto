9ff3e1c09099e41f452d9ca0fc5637f5cd267630
jesse.laflesch <jesse.laflesch@umontana.edu>
2019-11-20 14:51:57 -0700

    Started Organization login

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import './CSS/index.css';
-import Navbar from './Javascript/navbar'
-import ExitButton from './Javascript/exitbutton'
-import Homepage from './Javascript/homepage'
+import Navbar from './Javascript/navbar';
+import ExitButton from './Javascript/exitbutton';
+import Homepage from './Javascript/homepage';
+import Loginpage from './Javascript/loginpage';
 
 // const my_nav = Navbar;
 // const my_footer = ExitButton;
@@ -19,4 +20,4 @@ import Homepage from './Javascript/homepage'
 //  };
 
 //ReactDOM.render(<Navbar />, document.getElementById('root'));
-ReactDOM.render(<div><Navbar />, <Homepage />, <ExitButton /> </div>, document.getElementById('root'));
+ReactDOM.render(<div><Navbar />, <Loginpage /> </div>, document.getElementById('root'));
